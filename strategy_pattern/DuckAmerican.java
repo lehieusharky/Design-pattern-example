9b6630622573844fe2db7ac3b08de2945ce93f9c
lehieulttt <lehieultt@gmail.com>
2024-01-05 19:03:40 +0700

    [Strategy] Finish

---
+++ strategy_pattern/DuckAmerican.java
@@ -0,0 +1,8 @@
+public class DuckAmerican extends Duck {
+
+    @Override
+    public void swim() {
+        System.out.println("Swim in  American");
+    }
+
+}
---
+++ strategy_pattern/FlyInVietNamBehavior.java
@@ -0,0 +1,8 @@
+public class FlyInVietNamBehavior implements FlyBehavior {
+
+    @Override
+    public void fly() {
+        System.out.println("Fly in VietNam");
+    }
+
+}
